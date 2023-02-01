2590a0a6bc8d9e863e7adb5a03ed89a0b52e4fa5
Mario2044 <116796406+Mario2044@users.noreply.github.com>
2023-02-02 00:19:32 +0200

    fix

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
 let number = prompt ('Please enter three numbers separated by commas.');
 
-number.split(',');
+let numSplit =number.split(',');
 
-console.log(number);
+let num = numSplit.join('');
 
-let first = number[0];
+let first = num[0];
 
-let midlle = number[2];
+let midlle = num[1];
 
-let last = number[number.length - 1];
-
-alert(`first number is ${first}, last number is ${last}.`);
+let last = num[num.length - 1];
 
 let sum = +first + +midlle + +last;
-
-alert(`Пользователь ввел ${first} + ${midlle} + ${last} и получил ${sum}`);
 
 let min;
 let max;
 
-if (number[0] > number[2] && number[0] > number[4]) {
-    max = number[0];
-} else if (number[2] > number[0] && number[2] > number[4]) {
-    max = number[2];
-} else (number[4] > number[0] && number[4] > number[2]); {
-    max = number[4];
+alert(`first number is ${first}, last number is ${last}.`);
+
+alert(`Пользователь ввел ${first} + ${midlle} + ${last} и получил ${sum}`);
+
+
+if (num[0] > num[1] && num[0] > num[2]) {
+    max = num[0];
+} else if (num[1] > num[0] && num[1] > num[2]) {
+    max = num[1];
+} else (num[2] > num[0] && num[2] > num[1]); {
+    max = num[2];
 }
 
-if (number[0] < number[2] && number[0] < number[4]) {
-    min = number[0];
-} else if (number[2] < number[0] && number[2] < number[4]) {
-    min = number[2];
+if (num[0] < num[1] && num[0] < num[2]) {
+    min = num[0];
+} else if (num[1] < num[0] && num[1] < num[2]) {
+    min = num[1];
 }
 
 alert(`min is ${min}, max is ${max}`);
 
 let numOne = prompt("Please enter one of the numbers which you entered before?");
 
-if (number.indexOf(numOne) === -1) {
+if (numSplit.indexOf(numOne) === -1) {
 alert("Sorry, you did not enter this number!");
 }
 else {
-alert(number.indexOf(numOne));
+alert(numSplit.indexOf(numOne));
 }
